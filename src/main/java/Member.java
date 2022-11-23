e9a1a5d25e7442cb1ad8ec1fc5a984c9bdf62ad8
Hjordten <andr314e@stud.kea.dk>
2022-11-23 11:35:13 +0100

    Made constructor, getter, and setter.

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -11,9 +11,91 @@ public class Member {
     private boolean isActive;
     private boolean isCompetitive;
 
-    public Member() {}
+    public Member(String name, String address, String phoneNumber, String mail, Date birthday, String sex, Boolean isStudent, Boolean isActive, Boolean isCompetitive) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.mail = mail;
+        this.birthday = birthday;
+        this.sex = sex;
+        this.isStudent = isStudent;
+        this.isActive = isActive;
+        this.isCompetitive = isCompetitive;
+    }
 
     public String toString() {
         return null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public boolean isStudent() {
+        return isStudent;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public boolean isCompetitive() {
+        return isCompetitive;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setStudent(boolean student) {
+        isStudent = student;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public void setCompetitive(boolean competitive) {
+        isCompetitive = competitive;
     }
 }
