9e5fb99b034a8259a9a167ae72f43d9e034490aa
Jesper Nielsen <jesp74m8@stud.kea.dk>
2022-11-24 14:20:54 +0100

    Setting up test file for club.java

---
+++ src/test/java/datahandling/ClubTest.java
@@ -0,0 +1,14 @@
+package datahandling;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClubTest {
+
+    @org.junit.jupiter.api.Test
+    void searchMember() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteMember() {
+    }
+}
