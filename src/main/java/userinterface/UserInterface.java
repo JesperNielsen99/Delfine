68c953cbc583360e03d13ea34ad41bf9beaa6b7e
PrinceH00 <hamz0230@stud.kea.dk>
2022-11-24 13:41:34 +0100

    Rettelser: på printMainMenu(), start() og HandleMainMenuChoice.
    Tilføjelser: PrintEditMenu og exitProgram.

--- src/main/java/userinterface/UserInterface.java
+++ src/main/java/userinterface/UserInterface.java
@@ -10,33 +10,58 @@ public class UserInterface {
     private Scanner scanner = new Scanner(System.in).useLocale(Locale.GERMAN);
 
     public void start() {
+        System.out.println("Velkommen til Delfinen");
         printMainMenu();
+        handleMainMenuChoice();
     }
 
     public void printMainMenu() {
         System.out.println("""
+                Du har følgende valgmuligheder, hvad ønsker du at gøre?
+                                
                 1: Opret medlem 
-                2: Redigere Medlemmer
-                3: Søg efter medlemmer 
+                2: Søg efter medlem 
+                3: Redigere medlem 
                 4: Slet medlem """);
     }
 
     public void handleMainMenuChoice() {
-       
+        switch (readInt()) {
+            case 1 -> createMember();
+            case 2 -> searchMember();
+            case 3 -> editMember();
+            case 4 -> deleteMember();
+            case 5 -> exitProgram();
+        }
+
 
     }
 
     public void printEditMenu() {
         System.out.println("""
-                1:""");
+                Hvad ønsker du at redigere?
+                
+                1: Navn
+                2: Adresse 
+                3: Telefon nummer 
+                4: Mail
+                5: Fødselsdag
+                6: Køn
+                7: Studie status
+                8: Aktivitets status
+                9: Medlemskab 
+                10: Tilbage til hovedmenu  
+                """);
+
 
     }
 
-    public void handleEditMenuChoice() {
 
+    void handleEditMenuChoice() {
+         switch (readInt())
     }
 
-    public void createMember() {
+    void createMember() {
 
     }
 
@@ -50,6 +75,10 @@ public class UserInterface {
 
     public void deleteMember() {
 
+    }
+
+    public void exitProgram() {
+        System.exit(0);
     }
 
     public int readInt() {
